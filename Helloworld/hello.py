ecc9e789d5dcb5d81861cf9731c859dbe8485b52
Laura Johana Eugenio Rodriguez <78609488+JohanaER@users.noreply.github.com>
2021-09-14 08:32:20 -0500

    Update hello.py

--- Helloworld/hello.py
+++ Helloworld/hello.py
@@ -1,1 +1,9 @@
+import sys #improtar librerias 
 
+print('Hello, World!') #muestra texto en comillas 
+
+print('The sum of 2 and 3 is 5.')#imprime comentario 
+
+sum = int(sys.argv[1]) + int(sys.argv[2]) #realiza la suma 
+
+print('The sum of {0} and {1} is {2}.'.format(sys.argv[1], sys.argv[2], sum))#se imprime el resultado de la suma 
